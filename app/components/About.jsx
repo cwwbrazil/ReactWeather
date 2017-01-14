55331180eec165122a8a9087569832bc463e594d
Caleb <cwwbrazil@gmail.com>
2017-01-14 17:07:20 -0600

    Style about page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,8 +4,21 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>This page is an project for learning React.</p>
+      <h1 className="text-center">About</h1>
+      <p>This page is a project for learning React.</p>
+      <p>
+        <ul>
+          <li>
+            <p>This site is built with React and Foundation</p>
+          </li>
+          <li>
+            <a href="http://foundation.zurb.com/sites/docs/"> Foundation docs </a>
+          </li>
+          <li>
+            <a href="http://openweathermap.org">Open Weather Map</a> I used Open Weather Map to search for weather data by city name.
+          </li>
+        </ul>
+      </p>
     </div>
   )
 };
